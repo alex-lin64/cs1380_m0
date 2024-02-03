02f828cb971244128fc6f772a0fc9e1221b95700
alex-lin64 <alex_lin@brown.edu>
2024-02-03 02:43:52 +0000

    merge done

--- c/invert.sh
+++ c/invert.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 # Invert index to create a mapping from terms to URLs containing that term
 # The details of the index structure can be seen in the test cases
 
-url="$1"
+invert_url="$1"
 
 sed 's/^[ \t]*//;s/[ \t]*$//' | sort | uniq -c | while read -r count word; do
-    echo "$word | $count | $url"
+    echo "$word | $count | $invert_url"
 done
