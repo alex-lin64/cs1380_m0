7e5b49711a204b73e5f8a8c0aa6c6af81008350a
alex-lin64 <alex_lin@brown.edu>
2024-02-04 20:20:35 +0000

    tests

--- t/s_test_merge.sh
+++ t/s_test_merge.sh
@@ -9,9 +9,9 @@ DIFF=${DIFF:-diff}
 
 output_file="$T_FOLDER/output.txt"
 
-cat "$T_FOLDER"/d/m2.txt | c/merge.js d/m1_gi.txt > "$output_file"
+# cat "$T_FOLDER"/d/m2.txt | c/merge.js "$T_FOLDER"/d/m1_gi.txt > "$output_file"
 
-if $DIFF <(cat d/test_merge1.txt) <(cat "$T_FOLDER"/output.txt) > /dev/null;
+if $DIFF <(cat "$T_FOLDER"/d/test_merge1.txt) <(cat "$T_FOLDER"/d/m2.txt | c/merge.js "$T_FOLDER"/d/m1_gi.txt) > /dev/null;
 then
     echo "$0 success: custom merge indexes are identical"
 else
